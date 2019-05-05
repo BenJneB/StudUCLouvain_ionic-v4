bdeb6fb5bb96f4d0d4b78e3a604cf9044452e0d7
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-05-05 11:58:38 +0200

    hebdo

--- src/app/pages/studies/hebdo/hebdo.module.ts
+++ src/app/pages/studies/hebdo/hebdo.module.ts
@@ -3,12 +3,24 @@ import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { HebdoPage } from './hebdo';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HebdoPage],
   imports: [
-  	IonicModule,
-  	TranslateModule.forChild()
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule,
+  	CommonModule,
+    TranslateModule.forChild(),
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HebdoPage
+      }
+    ])
   ]
 })
 export class HebdoPageModule { }
