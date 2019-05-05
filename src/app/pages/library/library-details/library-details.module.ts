8e4a6749e93653b291bfd565edc4d4620f9fd94a
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-05-05 11:04:45 +0200

    libdetais

--- src/app/pages/library/library-details/library-details.module.ts
+++ src/app/pages/library/library-details/library-details.module.ts
@@ -3,12 +3,24 @@ import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { LibraryDetailsPage } from './library-details';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LibraryDetailsPage],
   imports: [
-  	IonicModule,
-  	TranslateModule.forChild()
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule,
+  	CommonModule,
+    TranslateModule.forChild(),
+    RouterModule.forChild([
+      {
+        path: '',
+        component: LibraryDetailsPage
+      }
+    ])
   ]
 })
 export class LibraryDetailsPageModule { }
