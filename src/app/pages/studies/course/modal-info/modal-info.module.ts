203f3a3a42d9cdc6a2f9199a892f59c67c259a16
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-05-05 11:54:15 +0200

    Fix

--- src/app/pages/studies/course/modal-info/modal-info.module.ts
+++ src/app/pages/studies/course/modal-info/modal-info.module.ts
@@ -3,12 +3,17 @@ import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { ModalInfoPage } from './modal-info';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ModalInfoPage],
   imports: [
-  	IonicModule,
-  	TranslateModule.forChild()
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule,
+  	CommonModule,
+    TranslateModule.forChild(),
   ]
 })
 export class ModalInfoPageModule { }
