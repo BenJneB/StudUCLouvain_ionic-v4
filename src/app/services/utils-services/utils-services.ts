2628ebc0deed271c45ec6abadc3d03c262bc0d6a
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-05-11 15:18:50 +0200

    Codeclimate style

--- src/app/services/utils-services/utils-services.ts
+++ src/app/services/utils-services/utils-services.ts
@@ -13,7 +13,7 @@ import { UserService } from './user-service';
         public user: UserService,
         private translateService: TranslateService,
         public alertCtrl: AlertController
-    ){
+    ) {
 
     }
 
@@ -33,27 +33,27 @@ import { UserService } from './user-service';
       /*Add an event to the favorites*/
   async addFavorite(slidingItem: IonItemSliding, itemData: any, texts: any, update: () => void) {
          if (this.user.hasFavorite(itemData.guid)) {
-            let message:string;
-            this.translateService.get(texts['FAV']).subscribe((res:string) => {message=res;});
+            let message: string;
+            this.translateService.get(texts['FAV']).subscribe((res: string) => {message = res;});
             this.removeFavorite(
                 slidingItem,
                 itemData,
-                message, 
+                message,
                 {
-                    'FAV3': texts['FAV3'], 
-                    'CANCEL': texts['CANCEL'], 
+                    'FAV3': texts['FAV3'],
+                    'CANCEL': texts['CANCEL'],
                     'DEL': texts['DEL']
                 },
                 update
             );
         } else {
             this.user.addFavorite(itemData.guid);
-            let message:string;
-            this.translateService.get(texts['FAV2']).subscribe((res:string) => {message=res;});
+            let message: string;
+            this.translateService.get(texts['FAV2']).subscribe((res: string) => {message = res;});
             const toast = await this.toastCtrl.create({
                 message: message,
                 duration: 3000
-            })
+            });
             await toast.present();
             await slidingItem.close();
         }
@@ -62,10 +62,10 @@ import { UserService } from './user-service';
 
   /*Remove an event from the favorites*/
     async  removeFavorite(slidingItem: IonItemSliding, itemData: any, title: string, texts: any, update: () => void) {
-            let message, cancel, del:string;
-            this.translateService.get(texts['FAV3']).subscribe((res:string) => {message=res;});
-            this.translateService.get(texts['CANCEL']).subscribe((res:string) => {cancel=res;});
-            this.translateService.get(texts['DEL']).subscribe((res:string) => {del=res;});
+            let message: string, cancel: string, del: string;
+            this.translateService.get(texts['FAV3']).subscribe((res: string) => {message = res;});
+            this.translateService.get(texts['CANCEL']).subscribe((res: string) => {cancel = res;});
+            this.translateService.get(texts['DEL']).subscribe((res: string) => {del = res;});
             const alert = await this.alertCtrl.create({
             header: title,
             message: message,
