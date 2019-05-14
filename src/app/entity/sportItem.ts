efd41b5888369a1c180f67b68755298d687a81dd
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-05-14 18:35:44 +0200

    2018-2019

--- src/app/entity/sportItem.ts
+++ src/app/entity/sportItem.ts
@@ -1,7 +1,7 @@
 /*
     Copyright (c)  Université catholique Louvain.  All rights reserved
     Authors :  Jérôme Lemaire and Corentin Lamy
-    Date : July 2017
+    Date : 2018-2019
     This file is part of Stud.UCLouvain
     Licensed under the GPL 3.0 license. See LICENSE file in the project root for full license information.  
 
