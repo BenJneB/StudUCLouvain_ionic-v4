3a7f880cf21288cb012545fd514d79d3f2636107
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-05-15 21:46:30 +0200

    fix

--- src/app/variables-config.ts
+++ src/app/variables-config.ts
@@ -1,2 +1,2 @@
-export var jsApiKey: string = 'YOUR_API_KEY';
-export var wso2HeaderStudent: string = 'Basic SDlfbE16UUVTYVBLbTNBbE53X25mQ2pUNl8wYTpEemtWOTVKUzh4ZVJiR2hMcEdEV1o0OURqRVlh';
+export var jsApiKey:string = "YOUR_API_KEY";
+export var wso2HeaderStudent:string = "AUTHORIZATION_HEADER_FOR_WSO2_LOGIN";
