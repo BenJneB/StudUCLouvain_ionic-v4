d093634a1da2939dc9c46314a26763ec61c0aad3
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-05-20 22:25:11 +0200

    Merging

--- src/app/variables-config.ts
+++ src/app/variables-config.ts
@@ -1,2 +1,2 @@
-export const jsApiKey = "YOUR_API_KEY";
-export const wso2HeaderStudent = "AUTHORIZATION_HEADER_FOR_WSO2_LOGIN";
+export let jsApiKey = 'YOUR_API_KEY';
+export let wso2HeaderStudent = 'AUTHORIZATION_HEADER_FOR_WSO2_LOGIN';
