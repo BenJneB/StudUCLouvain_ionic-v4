315657b11ebef8c5890078400c9a07e99da3fdae
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-05-21 22:47:59 +0200

    IABA

--- test-config/mocks-ionic.ts
+++ test-config/mocks-ionic.ts
@@ -1,4 +1,9 @@
+import { Observable, Observer } from 'rxjs';
+
 import { AppAvailability } from '@ionic-native/app-availability/ngx';
+import {
+    InAppBrowser, InAppBrowserObject, InAppBrowserOptions
+} from '@ionic-native/in-app-browser/ngx';
 import { Market } from '@ionic-native/market/ngx';
 
 export class MarketMock extends Market {
@@ -15,5 +20,57 @@ export class AppAvailabilityMock extends AppAvailability {
         return new Promise((resolve, reject) => {
             resolve(response);
         });
+    }
+}
+
+
+export interface InAppBrowserEvent extends Event {
+    /** the eventname, either loadstart, loadstop, loaderror, or exit. */
+    type: string;
+    /** the URL that was loaded. */
+    url: string;
+    /** the error code, only in the case of loaderror. */
+    code: number;
+    /** the error message, only in the case of loaderror. */
+    message: string;
+}
+
+export class InAppBrowserObjectMock extends InAppBrowserObject {
+
+    constructor(url: string, target?: string, options?: string | InAppBrowserOptions) {
+        super(url, target, options);
+    }
+
+    show(): void { }
+
+    close(): void { }
+
+    hide(): void { }
+
+    executeScript(script: { file?: string; code?: string; }): Promise<any> {
+        return new Promise((resolve, reject) => {
+            resolve();
+        });
+    }
+
+    insertCSS(css: { file?: string; code?: string; }): Promise<any> {
+        return new Promise((resolve, reject) => {
+            resolve();
+        });
+    }
+
+    on(event: string): Observable<InAppBrowserEvent> {
+        let response: InAppBrowserEvent;
+        return Observable.create((observer: Observer<any>) => {
+            observer.next(response);
+            observer.complete();
+        });
+    }
+}
+
+export class InAppBrowserMock extends InAppBrowser {
+    create(url: string, target?: string, options?: string | InAppBrowserOptions): InAppBrowserObjectMock {
+        const response = new InAppBrowserObjectMock(url);
+        return response;
     }
 }
