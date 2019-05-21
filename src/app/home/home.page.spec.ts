1419d544bc4c93631fe786fc2954a5f7b450c257
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-05-21 20:58:45 +0200

    Mock translate pipe test

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,7 +1,18 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePage } from './home.page';
+
+@Pipe({
+  name: 'translate'
+})
+export class TranslatePipeMock implements PipeTransform {
+  public name = 'translate';
+
+  public transform(query: string, ...args: any[]): any {
+    return query;
+  }
+}
 
 describe('HomePage', () => {
   let component: HomePage;
@@ -9,7 +20,7 @@ describe('HomePage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HomePage],
+      declarations: [HomePage, TranslatePipeMock],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
       .compileComponents();
