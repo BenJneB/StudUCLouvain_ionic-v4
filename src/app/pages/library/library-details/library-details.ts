82cf913ba8ff05351e137289fb584a0d7d761521
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-06-22 18:52:02 +0200

    Simplify

--- src/app/pages/library/library-details/library-details.ts
+++ src/app/pages/library/library-details/library-details.ts
@@ -58,8 +58,7 @@ export class LibraryDetailsPage {
       if (this.router.getCurrentNavigation().extras.state) {
         this.libDetails = this.router.getCurrentNavigation().extras.state.items;
         this.searching = true;
-        this.libService.loadLibDetails(this.libDetails).then(res => {
-        });
+        this.libService.loadLibDetails(this.libDetails);
         this.searching = false;
       }
     });
