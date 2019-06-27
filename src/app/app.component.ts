cef5e2412528c74e0baebbfcd0bf87fc29d67ee4
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-06-27 22:11:07 +0200

    remove styleUrls from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,7 +42,6 @@ import { UtilsService } from './services/utils-services/utils-services';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 
 
