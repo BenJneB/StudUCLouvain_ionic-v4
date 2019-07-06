8759ef1b5cec9b9906445b71088379e2f4424c9c
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-07-07 00:54:01 +0200

    Travis

--- src/app/pages/map/map.spec.ts
+++ src/app/pages/map/map.spec.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Diagnostic } from '@ionic-native/diagnostic/ngx';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { Network } from '@ionic-native/network/ngx';
 import { ModalController } from '@ionic/angular';
@@ -53,6 +54,7 @@ describe('Map Component', () => {
                 { provide: ModalController, useClass: ModalControllerMock },
                 { provide: InAppBrowser, useClass: InAppBrowserMock },
                 { provide: Network, useClass: NetworkMock },
+                Diagnostic,
             ]
         }).compileComponents();
     }));
