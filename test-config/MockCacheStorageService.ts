e4d6fe45d7bdf01f2e9d493bd9e9103e3deceaf3
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-09-21 05:31:01 +0200

    Remove useless

--- test-config/MockCacheStorageService.ts
+++ test-config/MockCacheStorageService.ts
@@ -1,32 +1,6 @@
-import { Storage, StorageConfig } from '@ionic/storage';
-
 export class MockCacheStorageService {
     constructor(a, b) { }
     public ready() {
         return true;
-    }
-}
-export class StorageMock extends Storage {
-    constructor(config: StorageConfig) {
-        super(config);
-    }
-    saveItem() {
-        return new Promise((resolve, reject) => {
-        });
-    }
-    getItem(key: string) {
-        return '';
-    }
-    removeItem(key: string) {
-        return new Promise((resolve, reject) => {
-        });
-    }
-    remove(key: string) {
-        return new Promise((resolve, reject) => {
-        });
-    }
-    set(key: string, value: any) {
-        return new Promise((resolve, reject) => {
-        });
     }
 }
