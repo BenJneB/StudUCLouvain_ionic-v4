5afe9678019f715c4b6c96fe2bd4a86094f249ff
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-10-20 21:52:56 +0200

    MockAdeService

--- test-config/MockStudiesService.ts
+++ test-config/MockStudiesService.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { StudiesService } from '../src/app/services/studies-services/studies-service';
 import { AdeService } from '../src/app/services/studies-services/ade-service';
+import { MockUtilsService } from './MockUtilsService';
 
 export class HttpClientMock extends HttpClient {
     constructor() {
@@ -9,13 +10,25 @@ export class HttpClientMock extends HttpClient {
 }
 
 export class MockStudiesService extends StudiesService {
-    constructor(http: HttpClientMock, ade: AdeService) {
+    constructor(http: HttpClientMock, ade: MockAdeService) {
         super(http, ade);
     }
 }
 
 export function newMockStudiesService() {
     let http: HttpClientMock;
-    let ade: AdeService;
+    let ade: MockAdeService;
     return new MockStudiesService(http, ade);
+}
+
+export class MockAdeService extends AdeService {
+    constructor(http: HttpClientMock, utils: MockUtilsService) {
+        super(http, utils);
+    }
+}
+
+export function newMockAdeServicee() {
+    let http: HttpClientMock;
+    let utils: MockUtilsService;
+    return new MockAdeService(http, utils);
 }
