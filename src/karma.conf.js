04d6ac452d69de8f5f725a2347995eca2cf77add
BenJneB <benjamin.daubry@student.uclouvain.be>
2019-10-24 23:14:00 +0200

    Require

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -2,36 +2,37 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-      frameworks: ['parallel', 'jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-firefox-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-spec-reporter')
-    ],
-    client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-      jasmine: {
-        random: false
-      },
-        captureConsole: false
+    config.set({
+        basePath: '',
+        frameworks: ['parallel', 'jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-parallel'),
+            require('karma-jasmine'),
+            require('karma-firefox-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma'),
+            require('karma-spec-reporter')
+        ],
+        client: {
+            clearContext: false, // leave Jasmine Spec Runner output visible in browser
+            jasmine: {
+                random: false
+            },
+            captureConsole: false
 
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['spec', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Firefox'],
-    singleRun: true,
-  });
+        },
+        coverageIstanbulReporter: {
+            dir: require('path').join(__dirname, '../coverage'),
+            reports: ['html', 'lcovonly', 'text-summary'],
+            fixWebpackSourcePaths: true
+        },
+        reporters: ['spec', 'kjhtml'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Firefox'],
+        singleRun: true,
+    });
 };
